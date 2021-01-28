65306d581e72d6b0383ed91e7f5b54623475d970
Muhammad Saiful Islam <muhammad@saiful.web.id>
2021-01-28 11:22:38 +0700

    git: Rename files to match default filenames

--- git/setup.sh
+++ git/setup.sh
@@ -12,7 +12,7 @@ if [ -z "$CONFIG_HOME" ]; then
     CONFIG_HOME=~/.config
 fi
 
-if test ~/dotfiles/git/gitconfig -ef $CONFIG_HOME/git/config && test ~/dotfiles/git/gitignore -ef $CONFIG_HOME/git/ignore; then
+if test ~/dotfiles/git/config -ef $CONFIG_HOME/git/config && test ~/dotfiles/git/ignore -ef $CONFIG_HOME/git/ignore; then
     echo "git found and configured already; skipping..."
     return
 fi
@@ -22,7 +22,7 @@ echo "git found and not configured; configuring..."
 mkdir -p $CONFIG_HOME/git
 
 rm -f $CONFIG_HOME/git/config
-ln -s ~/dotfiles/git/gitconfig $CONFIG_HOME/git/config
+ln -s ~/dotfiles/git/config $CONFIG_HOME/git/config
 
 rm -f $CONFIG_HOME/git/ignore
-ln -s ~/dotfiles/git/gitignore $CONFIG_HOME/git/ignore
+ln -s ~/dotfiles/git/ignore $CONFIG_HOME/git/ignore
